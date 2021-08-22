d927a3ecacf067a32d3c231f2493c1172490bcc6
roisholto <soetanrules@hotmail.com>
2021-08-22 02:17:13 +0100

    added tests

--- php/tests/AccountTest.php
+++ php/tests/AccountTest.php
@@ -24,10 +24,13 @@ final class AccountTest extends TestCase {
   */
   function testDeposit(){
     $Account = Account::getInstance() ;
-    $this->assertTrue($Account->deposit('victor', 10)) ;
-    $this->assertTrue($Account->deposit('darius', 20)) ;
-    $this->assertEquals($Account->get('victor')['amount'], 10) ;
-    $this->assertEquals($Account->get('darius')['amount'], 20) ;
+    $this->assertTrue($Account->deposit('victor', 10, 'USD')) ;
+    $this->assertTrue($Account->deposit('darius', 20, 'USD')) ;
+    $this->assertTrue($Account->deposit('darius', 1000, 'NGN')) ;
+
+    $this->assertEquals($Account->get('victor')['balance']['USD'], 10) ;
+    $this->assertEquals($Account->get('darius')['balance']['USD'], 20) ;
+    $this->assertEquals($Account->get('darius')['balance']['NGN'], 1000) ;
 
     return $Account ;
   }
@@ -37,7 +40,7 @@ final class AccountTest extends TestCase {
   */
   function testDepositException($Account){
     $this->expectException(\Exception::class) ;
-    $Account->deposit('victor', -200) ;
+    $Account->deposit('victor', -200, 'USD') ;
   }
 
   /**
@@ -45,10 +48,12 @@ final class AccountTest extends TestCase {
   */
   function testSendMoneyToPeer() {
     $Account = Account::getInstance() ;
-    $this->assertTrue($Account->sendMoneyToPeer('darius', 'victor', 15)) ;
+    $this->assertTrue($Account->sendMoneyToPeer('darius', 'victor', 15, 'USD')) ;
     // The balance of victor is expected to be 25
-    $this->assertEquals($Account->get('victor')['amount'], 25) ;
-    $this->assertEquals($Account->get('darius')['amount'], 5) ;
+    $this->assertEquals($Account->get('victor')['balance']['USD'], 25) ;
+    $this->assertEquals($Account->get('darius')['balance']['USD'], 5) ;
+
+    return $Account ;
   }
 
   /**
@@ -56,7 +61,7 @@ final class AccountTest extends TestCase {
   */
   function testSendMoneyToPeerException($Account){
     $this->expectException(\Exception::class) ;
-    $Account->sendMoneyToPeer('darius', 'victor', 15) ;
+    $Account->sendMoneyToPeer('darius', 'victor', 15, 'USD') ;
   }
 
   /**
@@ -65,19 +70,70 @@ final class AccountTest extends TestCase {
   function testTransferOutOfApp(){
     $Account = Account::getInstance() ;
     // succesfully transfer ;
-    $this->assertTrue($Account->transferOutOfApp('victor', 25)) ;
+    $this->assertTrue($Account->transferOutOfApp('victor', 25, 'USD')) ;
     // confirm that the amount was deducted after transferred
-    $this->assertEquals($Account->get('victor')['amount'], 0) ;
+    $this->assertEquals($Account->get('victor')['balance']['USD'], 0) ;
     return $Account ;
   }
 
+  /**
+  * @depends testDeposit
+  */
+  function testTransferMoneyToPeerNGNToUSD($Account){
+    // get darius
+    $darius_NGN = $Account->get('darius')['balance']['NGN'] ;
+    $deducted_from_darius_NGN = Account::moneyFormat(Account::convertCurrency(2, 'USD', 'NGN')) ;
+    $this->assertTrue($Account->sendMoneyToPeer('darius', 'victor', 7, 'USD')) ;
+    // though darius right now has just 5 dollar in his account
+    // he is able to send 7 dollars because he has funds in his NGN account
+    $this->assertEquals($Account->get('victor')['balance']['USD'], 7) ;
+    $this->assertEquals($Account->get('darius')['balance']['USD'], 0) ;
+    $this->assertEquals($Account->get('darius')['balance']['NGN'], $darius_NGN - $deducted_from_darius_NGN) ;
+
+    return $Account ;
+  }
+
+  /**
+  * @depends testTransferMoneyToPeerNGNToUSD
+  */
+  function testTransferMoneyOutUSDToNGN($Account){
+    // transfer money to thirdparty in NGN from USD
+    // currently victor has just 7 dollar in in his USD account
+    $this->assertTrue($Account->transferOutOfApp('victor', 800, 'NGN')) ;
+
+  }
+
+  /**
+  * @depends testDeposit
+  */
+  function testTransferMoneyFromYENToNGN($Account){
+    $yen_to_ngn = Account::moneyFormat(Account::convertCurrency(1500.00, 'YEN', 'NGN') ) ;
+    echo 'yy ', $yen_to_ngn, "\r\n";
+    $Account->deposit('victor', 5000, 'YEN') ;
+    // Ensure that victor has nothing left in his dollar account just to make the
+    // the values more obvious.
+    $Account->sendMoneyToPeer('victor', 'darius', 5.06, 'USD') ;
+    $Account->sendMoneyToPeer('victor', 'darius', $yen_to_ngn, 'NGN') ;
+    //
+    $this->assertEquals($Account->get('darius')['balance']['USD'], 5.06) ;
+    // be sure that the we have 2000 yen left in victors account ;
+    $this->assertEquals($Account->get('victor')['balance']['YEN'], 3500.00) ;
+
+    print_r($Account->get('darius')['balance']) ;
+
+    // now ensure we have the naira equivalent of 3000 yen in darius account ;
+    $this->assertEquals(
+      $Account->get('darius')['balance']['NGN'],
+      $yen_to_ngn
+      ) ;
+  }
   /**
   * @depends testTransferOutOfApp
   */
   function testTransferOutOfAppExceptionNegativeAmount($Account){
     // failed transfer becuase of negative amount
     $this->expectException(\Exception::class) ;
-    $Account->transferOutOfApp('victor', -25) ;
+    $Account->transferOutOfApp('victor', -1005, 'USD') ;
   }
 
   /**
@@ -86,15 +142,18 @@ final class AccountTest extends TestCase {
   function testTransferOutOfAppExceptionInsufficientBalance($Account){
     // attempting to transfer more than is available ;
     $this->expectException(\Exception::class) ;
-    $Account->transferOutOfApp('darius', 200) ;
+    $Account->transferOutOfApp('darius', 800, 'USD') ;
   }
 
 
-  /**
-  *
-  */
-  function getAccount(){
+  function testConvertNGNToUSD(){
+    $this->assertEquals(Account::moneyFormat(Account::convertCurrency(1000, 'NGN', 'USD')), 2.43) ;
+  }
 
+  function testConvertNGNToYUAN(){
+    $this->assertEquals(
+      Account::moneyFormat(Account::convertCurrency(3000, 'NGN', 'YUAN')),
+      797.94) ;
   }
 
 }
